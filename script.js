1947ac2a4d9c0fe72c9cc48857c9b54d96adc87a
wllt7009-eng <wllt7009@gmail.com>
2026-04-24 20:15:57 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+// Efeito de revelar ao rolar a página
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 100;
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal);
+window.onload = reveal;
